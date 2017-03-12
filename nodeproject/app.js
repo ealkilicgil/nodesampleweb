5fb4c7395f892582295d23c28212bd11cfd29c7a
ealkilicgil <ealkilicgil@gmail.com>
2017-03-12 10:34:44 +0000

    app.js

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,17 @@
-var aloha='Hello';
-console.log(aloha);
+var express=require("express");
+var app=express();
+
+var port=process.env.PORT;
+
+app.get('/',function(request,response){
+   response.send('Helloooooo!!!'); 
+    
+});
+app.get('/routing',function(request,response){
+   response.send('Helloooooo routing!!!'); 
+    
+});
+app.listen(port,function(err){
+    
+    console.log('Server is running on port: '+port);
+});
